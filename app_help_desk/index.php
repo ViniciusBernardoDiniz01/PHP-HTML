2c598b0e7f2d04752abc6f18feab4f76cecf7a09
Vinicius <vinicius.diniz.senai404@gmail.com>
2024-09-20 09:47:50 -0300

    Add files via upload

---
+++ app_help_desk/index.php
@@ -0,0 +1,44 @@
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>App Help Desk</title>
+
+    
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" href="bootstrap.css">
+    </style>
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">
+        <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+        App Help Desk
+      </a>
+    </nav>
+
+    <div class="container">    
+      <div class="row">
+
+        <div class="card-login">
+          <div class="card">
+            <div class="card-header">
+              Login
+            </div>
+            <div class="card-body">
+              <form>
+                <div class="form-group">
+                  <input type="email" class="form-control" placeholder="E-mail">
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control" placeholder="Senha">
+                </div>
+                <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
+              </form>
+            </div>
+          </div>
+        </div>
+    </div>
+  </body>
+</html>
