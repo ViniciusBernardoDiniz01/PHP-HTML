b67682d9a09bd4344ab613b8935d94dfcf3e4e0c
Vinicius <vinicius.diniz.senai404@gmail.com>
2024-09-20 09:25:48 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php $nome = 'Valter';?>
+
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Olá bem-vindo de volta <?php echo $nome;?></h1>
+</body>
+</html>
